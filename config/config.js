f1f7d71d7646ca674daac9a717a51e0c759445ed
Alexander Savin <box.for.postman@gmail.com>
2014-02-08 20:12:01 +0400

    files that start with _ are readonly

--- config/config.js
+++ config/config.js
@@ -15,6 +15,6 @@ module.exports = {
             'node_modules'
         ],
         r: /^(\.bem|desktop\.b|package\.json)((?!(\.sock\b|\bcache\b)).)*$/,
-        w: /^desktop\.b((?!(\/\.bem)).)*$/
+        w: /^desktop\.b((?!(\/\.bem|\/_)).)*$/
     }
 };
