a00910960b6d487ad4b06f080522ac4f301d3710
Alexander Savin <box.for.postman@gmail.com>
2014-03-06 22:50:13 +0400

    Error handling improved

--- index.js
+++ index.js
@@ -1,46 +1,23 @@
 var express = require('express'),
     project = require('./lib/project'),
     bemConfig = require('./config/bem'),
+    handleErrors = require('./lib/error').handleErrors,
     app = express();
 
 app.use(express.bodyParser());
 
 app.post('/project', function (req, res) {
-    project.create(function (err, id) {
-        if (err) {
-            res.send(500, err);
-        }
-        //todo: replace with 201
-        res.send(200, {id: id});
-    })
+    project.create(handleErrors(res, function (id) {
+        res.send(201, {id: id});
+    }));
 });
-
-/**
- * @param {Error} err
- * @param {http.ServerResponse} res
- * @param {String} projectId
- * @param {String} [path]
- */
-function handleFileError (err, res, projectId, path) {
-    if (err.message === '404' || err.message === '403') {
-        var response = path ? projectId + '/' + path : projectId;
-        res.send(err.message, response);
-    } else {
-        console.log(err);
-        res.send(500, err.message);
-    }
-}
 
 function onGetProjectFileRequest (req, res) {
     var projectId = req.params[0],
         path = req.params[1];
-    project.get(projectId, path, function (err, data) {
-        if (err) {
-            handleFileError(err, res, projectId, path);
-            return;
-        }
+    project.get(projectId, path, handleErrors(res, function (data) {
         res.send(data);
-    });
+    }));
 }
 
 var projectRegExp = /^\/project\/([\w-]+)$/,
@@ -49,16 +26,12 @@ var projectRegExp = /^\/project\/([\w-]+)$/,
 app.get(projectRegExp, onGetProjectFileRequest);
 app.post(projectRegExp, function (req, res) {
     var projectId = req.params[0],
-        callback = function (err, id, queueNumber) {
-            if (err) {
-                handleFileError(err, res, projectId);
-                return;
-            }
-            res.send(200, {
+        callback = handleErrors(res, function (id, queueNumber) {
+            res.send({
                 id: id,
                 queue: queueNumber
             });
-        };
+        });
     switch (req.param('action')) {
         case 'build':
             project.build(projectId, callback);
@@ -70,24 +43,20 @@ app.post(projectRegExp, function (req, res) {
             project.status(projectId, req.param('method'), parseInt(req.param('queue')), callback);
             break;
         default :
-            res.send(400, 'No such action');
+            res.send(400, 'No such action: ' + req.param('action'));
     }
 });
 
 
 app.get(projectResourceRegExp, onGetProjectFileRequest);
 app.post(projectResourceRegExp, function (req, res) {
-    project.write(req.params[0], req.params[1], req.body.content, function (err, data) {
-        if (err) {
-            handleFileError(err, res, req.param[0], req.param[1]);
-            return;
-        }
+    project.write(req.params[0], req.params[1], req.body.content, handleErrors(res, function (data) {
         res.send(data);
-    });
+    }));
 });
 
 app.get('/techs', function (req, res) {
-    res.send(200, bemConfig.availableTechs);
+    res.send(bemConfig.availableTechs);
 });
 
 app.post('/block', function (req, res) {
@@ -95,18 +64,9 @@ app.post('/block', function (req, res) {
     bemConfig.declKeys.forEach(function (param) {
         params[param] = req.param(param);
     });
-    project.createBemEntity(req.param('projectId'), params, function (err, data) {
-        if (err) {
-            if (err.message === '400') {
-                res.send(err.message, 'Wrong arguments');
-            } else {
-                console.log(err);
-                res.send(500, err.message);
-            }
-            return;
-        }
-        res.send(200, data);
-    });
+    project.createBemEntity(req.param('projectId'), params, handleErrors(res, function (data) {
+        res.send(data);
+    }));
 });
 
 app.listen(3001);
